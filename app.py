d7d3767b72fc1a16f348cd4e03ccffe0c6ab3b04
manpreet maan <visionaryempirex@gmail.com>
2024-12-18 12:45:23 +0530

    traceback added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify, send_from_directory
 import io
 import json
 # Import document processing functions
+import traceback
 from DocumentScraper import (
     passport, labor_card, residence_visa,
     emirates_id, home_country_id, labor_contract_information
@@ -30,40 +31,44 @@ def document_scraping():
 
     # Parse JSON data from the request
     # access headers
-    headers = request.headers
-  
-    if 'X-API-KEY' not in headers:
-        return jsonify({"error": "API key is missing."}), 401
+    try:
+        headers = request.headers
     
-    if headers['X-API-KEY'] != os.environ.get('API_KEY'):
-        return jsonify({"error": "Invalid API key."}), 401
-    query_parameters = request.args
-    if 'document_type' not in query_parameters:
-        return jsonify({"error": "document_type is missing."}), 400
-    
-    document_type = query_parameters['document_type']
-    
-    file = request.files.get('file')
-    
-
-    # Validate the document type
-    if not document_type or document_type not in document_handlers:
-
-        return jsonify({"error": "Invalid or unsupported document_type."}), 400
-
-    # Get the handler function for the document type
-    handler_function = document_handlers[document_type]
-    file_content = io.BytesIO(file.read())
-
-    # Pass the file-like object to the handler function
-    handler_function = document_handlers[document_type]
-    
-    resp= handler_function(file_content, file.mimetype.startswith("image")).content
-    print(resp)
-    # Call the handler function with the appropriate arguments
-
-    return jsonify(json.loads(resp)), 200
-    
+        if 'X-API-KEY' not in headers:
+            return jsonify({"error": "API key is missing."}), 401
         
+        if headers['X-API-KEY'] != os.environ.get('API_KEY'):
+            return jsonify({"error": "Invalid API key."}), 401
+        query_parameters = request.args
+        if 'document_type' not in query_parameters:
+            return jsonify({"error": "document_type is missing."}), 400
+        
+        document_type = query_parameters['document_type']
+        
+        file = request.files.get('file')
+        
+
+        # Validate the document type
+        if not document_type or document_type not in document_handlers:
+
+            return jsonify({"error": "Invalid or unsupported document_type."}), 400
+
+        # Get the handler function for the document type
+        handler_function = document_handlers[document_type]
+        file_content = io.BytesIO(file.read())
+
+        # Pass the file-like object to the handler function
+        handler_function = document_handlers[document_type]
+        
+        resp= handler_function(file_content, file.mimetype.startswith("image")).content
+        print(resp)
+        # Call the handler function with the appropriate arguments
+
+        return jsonify(json.loads(resp)), 200
+    except Exception as e:
+        
+        return traceback.format_exc(), 500
+        
+            
 if __name__ == '__main__':
     app.run()
