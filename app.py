a77740eb3c0782d96697f652fe83807232941539
manpreet maan <visionaryempirex@gmail.com>
2024-12-18 11:35:11 +0530

    added yaml ad other importventss

--- app.py
+++ app.py
@@ -1,32 +1,69 @@
 import os
-
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import Flask, request, jsonify, send_from_directory
+import io
+import json
+# Import document processing functions
+from DocumentScraper import (
+    passport, labor_card, residence_visa,
+    emirates_id, home_country_id, labor_contract_information
+)
 
 app = Flask(__name__)
-# test
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+# Map document types to their corresponding functions
+document_handlers = {
+    'passport'                  : passport,
+    'labor_card'                : labor_card,
+    'residence_visa'            : residence_visa,
+    'emirates_id'               : emirates_id,
+    'home_country_id'           : home_country_id,
+    'labor_contract_information': labor_contract_information
+}
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+@app.route('/DocumentScraping', methods=['POST'])
+def document_scraping():
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    # Parse JSON data from the request
+    # access headers
+    headers = request.headers
+  
+    if 'X-API-KEY' not in headers:
+        return jsonify({"error": "API key is missing."}), 401
+    
+    if headers['X-API-KEY'] != os.environ.get('API_KEY'):
+        return jsonify({"error": "Invalid API key."}), 401
+    query_parameters = request.args
+    if 'document_type' not in query_parameters:
+        return jsonify({"error": "document_type is missing."}), 400
+    
+    document_type = query_parameters['document_type']
+    
+    file = request.files.get('file')
+    
 
+    # Validate the document type
+    if not document_type or document_type not in document_handlers:
 
+        return jsonify({"error": "Invalid or unsupported document_type."}), 400
+
+    # Get the handler function for the document type
+    handler_function = document_handlers[document_type]
+    file_content = io.BytesIO(file.read())
+
+    # Pass the file-like object to the handler function
+    handler_function = document_handlers[document_type]
+    
+    resp= handler_function(file_content, file.mimetype.startswith("image")).content
+    print(resp)
+    # Call the handler function with the appropriate arguments
+
+    return jsonify(json.loads(resp)), 200
+    
+        
 if __name__ == '__main__':
-   app.run()
+    app.run(port=5001, debug=True)
